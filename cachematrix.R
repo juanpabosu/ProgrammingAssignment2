f1a48fa393f01f62ffd12afff106efe97e97d02b
juanpabosu <jbsuaza@gmail.com>
2014-06-05 14:42:45 -0500

    Update with R code for assignment: Caching the inverse of a matrix, part of coursera Programming with R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,40 @@
 
 ## Write a short comment describing this function
 
+## Set function create a matrix with Y numeric values, ROWS and COLS represents
+## the matrix dimension. MINV represents the variable for the inverse matrix of X
 makeCacheMatrix <- function(x = matrix()) {
+  
+  minv <- NULL
+  set <- function(y=numeric(), rows=numeric(), cols=numeric()) {
+    x <<- matrix(y, nrow=rows, ncol=cols)
+    minv <<- NULL
+  }
+  get <- function() x
+  setminv <- function(minverse) minv <<- minverse
+  getminv <- function() minv
+  list(set = set, get = get,
+       setminv = setminv,
+       getminv = getminv)
 
 }
 
 
 ## Write a short comment describing this function
 
+## Library MASS contains the function ginv(X) that calculates 
+## the Moore-Penrose generalized inverse of a matrix X.
+
+library("MASS") 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  minv <- x$getminv()
+  if(!is.null(minv)) {
+    message("getting cached inverse matrix")
+    return(minv)
+  }
+  matrix_data <- x$get()
+  minv <- ginv(matrix_data, ...)
+  x$setminv(minv)
+  minv
 }
